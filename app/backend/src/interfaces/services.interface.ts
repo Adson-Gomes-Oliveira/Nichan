69f8a2ae67dc90a488e3339b6b990c9e7bd4aba9
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-11-19 20:53:15 -0300

    <feat>: Model and Service interface created

---
+++ app/backend/src/interfaces/services.interface.ts
@@ -0,0 +1,9 @@
+interface IServices<T> {
+  create(payload: T): Promise<T>;
+  findAll(): Promise<T[]>;
+  findOne(_id: string): Promise<T | null>;
+  update(_id: string, payload: T): Promise<T | null>;
+  delete(_id: string): Promise<T | null>;
+}
+
+export default IServices;
