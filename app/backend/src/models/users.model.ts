7546b84c02ab8d4e9d6bc9db7a555aa098d88f58
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-11-20 10:30:00 -0300

    <feat>: Created a model for users

---
+++ app/backend/src/models/users.model.ts
@@ -0,0 +1,20 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { IUser } from './../interfaces/user.interface';
+import MongoModel from './mongo.model';
+
+const userMongooseSchema = new Schema<IUser>({
+  nickName: String,
+  fullName: String,
+  email: String,
+  password: String,
+  gender: String,
+  user_animes_list: Array,
+}, { versionKey: false });
+
+class UsersModel extends MongoModel<IUser> {
+  constructor(public model = mongooseCreateModel('User', userMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default UsersModel;
