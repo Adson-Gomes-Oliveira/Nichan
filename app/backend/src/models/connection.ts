f6a1a0064655173361ada34cfe50c95ef1189875
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-11-23 00:20:27 -0300

    <fix>: Adding user and password of DB to mongoose

--- app/backend/src/models/connection.ts
+++ app/backend/src/models/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Nichan';
+const MONGO_DB_URL = 'mongodb://root:password@localhost:27017/Nichan';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
