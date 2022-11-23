fd217e894b8f06b17dbc25b2674c3e4d7efacb0d
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-11-23 00:16:27 -0300

    <feat>: Connecting project to mongoose

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,6 +1,16 @@
 import app from './app';
 import 'dotenv/config';
+import connectToDatabase from './models/connection';
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log('Running Nichan Server on 3001'));
+connectToDatabase()
+  .then(() => {
+    app.listen(port, () => console.log('Running Nichan Server on 3001'));
+  })
+  .catch((error) => {
+    console.log('Connection with database generated an error:\r\n');
+    console.error(error);
+    console.log('\r\nServer initialization cancelled');
+    process.exit(0);
+  });
