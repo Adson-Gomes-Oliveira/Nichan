e4cf4522b147620515e05b75fd587d4991f80855
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2022-12-10 18:33:54 -0300

    <feat>: Created page structure of anime details

--- app/frontend/src/pages/AnimeDetails.tsx
+++ app/frontend/src/pages/AnimeDetails.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import IAnime from '../interfaces/anime.interface';
+
 import { findOneExternalAnime } from '../services/animesExternalRequest';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import IAnime from '../interfaces/anime.interface';
+
+import './css/animeDetails.css';
 
 function AnimeDetails(): JSX.Element {
   const { id } = useParams();
-  const [animeDetails, setAnimeDetails] = useState({});
+  const [animeDetails, setAnimeDetails] = useState<IAnime>();
  
   const getAnimeDetails = useCallback(async (): Promise<void> => {
     const request = await findOneExternalAnime(id as string);
@@ -17,8 +22,35 @@ function AnimeDetails(): JSX.Element {
   }, [getAnimeDetails]);
 
   return (
-    <section>
-      
+    <section className="anime-details-section">
+      <Header />
+      {animeDetails && (
+        <div className="details-anime">
+          <span>{animeDetails.title}</span>
+          <div className="description-details">
+            <img src={animeDetails.cover} alt={`Anime - ${animeDetails.title}`} />
+            <div className="text-description">
+              <h3>Sobre o anime</h3>
+              <p>{animeDetails.description}</p>
+            </div>
+          </div>
+          <div className="infos">
+            <span>Nota geral: {animeDetails.rating}</span>
+            <span>Genêros: {animeDetails.genres.toString()}</span>
+            <span>Data de Lançamento: {animeDetails.releaseDate}</span>
+          </div>
+        </div>
+      )}
+      <button
+        type="button"
+      >
+        <span>Adicionar a lista de Favoritos +</span>
+      </button>
+      <div className="comments-anime">
+        <h2>Comentários</h2>
+        <div className="coment-card"></div>
+      </div>
+      <Footer />
     </section>
   );
 }
